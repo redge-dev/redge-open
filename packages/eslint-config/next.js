3fc7f99075e4966c9dc752d8d71c8427db217ad9
Thinh Trinh Duc <thinh.duc.trinh@gmail.com>
2024-11-07 10:23:20 +0700

    Initialize repo

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,14 +1,17 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "turbo",
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+    require.resolve('@vercel/style-guide/eslint/next'),
+    'turbo',
   ],
   globals: {
     React: true,
@@ -18,18 +21,28 @@ module.exports = {
     node: true,
     browser: true,
   },
-  plugins: ["only-warn"],
+  plugins: ['only-warn'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
+    react: {
+      version: 'detect',
+    },
   },
   ignorePatterns: [
     // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
+    '.*.js',
+    'node_modules/',
   ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-};
+  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
+  rules: {
+    'react/self-closing-comp': 'error',
+    'react/prop-types': 'off',
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+  },
+  parser: "@typescript-eslint/parser",
+}
--- packages/eslint-config/react-internal.js
+++ packages/eslint-config/react-internal.js
@@ -1,6 +1,6 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path')
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json')
 
 /*
  * This is a custom ESLint configuration for use with
@@ -10,30 +10,45 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "turbo"],
-  plugins: ["only-warn"],
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'eslint:recommended',
+    'prettier',
+    'turbo',
+  ],
+  plugins: ['only-warn', '@typescript-eslint'],
   globals: {
     React: true,
     JSX: true,
   },
   env: {
     browser: true,
+    node: true,
   },
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
+    react: {
+      version: 'detect',
+    },
   },
   ignorePatterns: [
     // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
+    '.*.js',
+    'node_modules/',
+    'dist/',
   ],
   overrides: [
     // Force ESLint to detect .tsx files
-    { files: ["*.js?(x)", "*.ts?(x)"] },
+    { files: ['*.js?(x)', '*.ts?(x)'] },
   ],
-};
+  rules: {
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+  },
+  parser: "@typescript-eslint/parser",
+}
